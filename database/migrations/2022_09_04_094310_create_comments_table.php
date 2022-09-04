bf35fc88278d76b5bd99aed75bb5c0e75afb23f1
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-09-04 18:54:15 +0900

    migeation実行

--- database/migrations/2022_09_04_094310_create_comments_table.php
+++ database/migrations/2022_09_04_094310_create_comments_table.php
@@ -15,6 +15,11 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string("body");
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("post_id");
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
             $table->timestamps();
         });
     }
