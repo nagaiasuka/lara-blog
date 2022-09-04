0213d92cf444586056140df508db0888e8827d27
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-09-04 15:23:51 +0900

    index機能実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+// Postの読み込み宣言
+use App\Post;
 
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        // データベースからデータの取り出し
+        $posts = Post::all();
+        // dd($posts);
+        return view('posts.index',compact('posts'));
+    }
 }
